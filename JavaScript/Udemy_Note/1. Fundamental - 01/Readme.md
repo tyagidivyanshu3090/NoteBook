982a993436a57db936d2e651c46028caf3e909e8
Divyanshu Tyagi <divyanshutyagi@Divyanshus-MacBook-Pro.local>
2025-03-11 18:03:00 +0530

    Including the Js file in the html

---
+++ JavaScript/Udemy_Note/1. Fundamental - 01/Readme.md
@@ -0,0 +1,2 @@
+# commit 1
+- Infusing the script code in the html using the script tag
---
+++ JavaScript/Udemy_Note/1. Fundamental - 01/script1.js
@@ -0,0 +1,4 @@
+// ? Commit 1 -> using the alert on the webpage
+let js = "amazing";
+if (js = "amazing") alert("javaScript is easy to learn")
+
