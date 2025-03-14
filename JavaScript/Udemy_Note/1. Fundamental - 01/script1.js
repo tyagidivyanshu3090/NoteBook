421f976b0270d881a60e4d8338e5e506c8919bcb
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-03-15 00:42:39 +0530

    typeof operator

--- JavaScript/Udemy_Note/1. Fundamental - 01/script1.js
+++ JavaScript/Udemy_Note/1. Fundamental - 01/script1.js
@@ -2,8 +2,16 @@
 // let js = "amazing";
 // if (js = "amazing") alert("javaScript is easy to learn")
  
-// ? Commit 2 -> 
+// ? Commit 2 -> DataType & typeOf operator
 
-let name = "Divyanshu";
+let PI = 3.14;  // * Snake convention
+let javascriptIsFun = true;
+console.log(typeof PI);
+console.log(typeof 42); // "number"
+console.log(typeof "Hello"); // "string"
+console.log(typeof true); // "boolean"
+console.log(typeof {}); // "object"
+console.log(typeof undefined); // "undefined"
+
 
 
