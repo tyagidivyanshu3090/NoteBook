40bca51812695952d9d5623b38a725952ee79415
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-04-12 13:55:48 +0530

    Increase the size of threadPool from 4 to 8

--- Node/4. Thread-pool/threadPool.cjs
+++ Node/4. Thread-pool/threadPool.cjs
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const crypto = require("crypto");
 
+// process.env.UV_THREADPOOL_SIZE = 8; // * Changing the size of thread pool
 crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
   console.log("1 - cryptoPBKDF2 done");
 });
