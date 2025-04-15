9e450a78fa99d0971ea22b783967f6fdeeff757a
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-04-15 18:25:09 +0530

    No number added Cases

--- JavaScript/Udemy_Note/4. Browser Manipulation/Guess-The-Number/script.js
+++ JavaScript/Udemy_Note/4. Browser Manipulation/Guess-The-Number/script.js
@@ -1,7 +1,14 @@
 "use strict";
 
-// Selecting the element in javaScript
-/*
- * console.log(document.querySelector(".message").textContent);
- * the output is Start guessing
- */
+// * console.log(document.querySelector(".message").textContent);
+// * console.log(document.querySelector(".number").textContent);
+// * console.log(document.querySelector(".score").textContent);
+// * document.querySelector(".guess").value = 17;
+
+function handleClick() {
+  const guessNumber = Number(document.querySelector(".guess").value);
+  if (!guessNumber) {
+    document.querySelector(".message").textContent = "Please enter the number";
+  }
+}
+document.querySelector(".check").addEventListener("click", handleClick);
