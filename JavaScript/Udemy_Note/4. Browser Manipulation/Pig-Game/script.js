b916e70403f4ede7c9fba8b092b9d86d0d98e144
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-04-21 16:56:56 +0530

    Displaying the score

--- JavaScript/Udemy_Note/4. Browser Manipulation/Pig-Game/script.js
+++ JavaScript/Udemy_Note/4. Browser Manipulation/Pig-Game/script.js
@@ -1,7 +1,8 @@
 "use strict";
 
 // * Selecting the intial element to implement initial condition
-
+let player0El = document.querySelector(".player--0");
+let player1El = document.querySelector(".player--1");
 let score0El = document.querySelector("#score--0");
 let score1El = document.getElementById("score--1");
 let current0El = document.getElementById("current--0");
@@ -11,9 +12,12 @@ let btnNew = document.querySelector(".btn--new");
 let btnRoll = document.querySelector(".btn--roll");
 let btnHold = document.querySelector(".btn--hold");
 
+let activePlayer = 0;
+const scores = [0, 0]; // Since array is zero based index -> hence we have 2 players  with id as current--0 & current--1
+
 // Implementing the initial condition
-score0El.textContent = 0;
-score1El.textContent = 0;
+score0El.textContent = scores[0];
+score1El.textContent = scores[1];
 diceEl.classList.add("hidden");
 let currentScore = 0;
 
@@ -31,10 +35,25 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     // Not equal to 1 -> add dice value to currentScore
     currentScore += dice;
-    console.log(currentScore);
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    // Switch to next player
+    // Switch to next player but before that:
+    // 1. assigning the value of currentScore and earlier score to the display
+    // 2. setting the currentScore of prev player to zero
+    // Update the total score before switching
+    scores[activePlayer] += currentScore;
+
+    // Correct way to update the score on screen
+    if (activePlayer === 0) {
+      score0El.textContent = scores[0];
+    } else {
+      score1El.textContent = scores[1];
+    }
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0; // Shifting the active player
     currentScore = 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
