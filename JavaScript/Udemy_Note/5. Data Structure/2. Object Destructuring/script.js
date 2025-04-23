f7da5479c6f7eb44d8037ad0f7efc493aa267fe8
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-04-23 18:21:48 +0530

    Nested Object Destructuring

--- JavaScript/Udemy_Note/5. Data Structure/2. Object Destructuring/script.js
+++ JavaScript/Udemy_Note/5. Data Structure/2. Object Destructuring/script.js
@@ -1,13 +1,13 @@
 //  ? Basic Object Destructuring
 
-// const restaurant = {
-//   name: "Classico Italiano",
-//   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-//   openingHours: {
-//     fri: { open: 11, close: 23 },
-//     sat: { open: 12, close: 24 },
-//   },
-// };
+const restaurant = {
+  name: "Classico Italiano",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  openingHours: {
+    fri: { open: 11, close: 23 },
+    sat: { open: 12, close: 24 },
+  },
+};
 
 // const { name, categories, openingHours } = restaurant;
 // console.log(name); // 'Classico Italiano'
@@ -30,3 +30,15 @@
 
 // console.log(menu); // [] (default value because 'menu' doesn't exist)
 // console.log(starters); // Undefined unless `starterMenu` exists in restaurant
+
+// ? Nested Object Destructuring
+// const {
+//   fri: { open, close },
+// } = restaurant.openingHours;
+// console.log(open, close); // 11 23
+
+// // With variable renaming
+// const {
+//   fri: { open: o, close: c },
+// } = restaurant.openingHours;
+// console.log(o, c); // 11 23
