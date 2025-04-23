c866391ac69322a3691cf44f32150c29cf08537d
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-04-23 18:18:27 +0530

    Destructuring with Renaming

--- JavaScript/Udemy_Note/5. Data Structure/2. Object Destructuring/script.js
+++ JavaScript/Udemy_Note/5. Data Structure/2. Object Destructuring/script.js
@@ -1,15 +1,26 @@
 //  ? Basic Object Destructuring
 
-const restaurant = {
-  name: "Classico Italiano",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  openingHours: {
-    fri: { open: 11, close: 23 },
-    sat: { open: 12, close: 24 },
-  },
-};
+// const restaurant = {
+//   name: "Classico Italiano",
+//   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+//   openingHours: {
+//     fri: { open: 11, close: 23 },
+//     sat: { open: 12, close: 24 },
+//   },
+// };
 
-const { name, categories, openingHours } = restaurant;
-console.log(name); // 'Classico Italiano'
-console.log(categories); // ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']
-console.log(openingHours); // { fri: {...}, sat: {...} }
+// const { name, categories, openingHours } = restaurant;
+// console.log(name); // 'Classico Italiano'
+// console.log(categories); // ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']
+// console.log(openingHours); // { fri: {...}, sat: {...} }
+
+// ?  Destructuring with Renaming
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+
+// console.log(restaurantName); // 'Classico Italiano'
+// console.log(hours); // { fri: {...}, sat: {...} }
+// console.log(tags); // ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']
