9682484e5d52835d787a8843fa4a567fa114073a
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-04-28 15:01:20 +0530

    Creating the server

---
+++ Node/5. CreatingTheServer/Readme.md
@@ -0,0 +1,11 @@
+# Creating the http server:
+
+- Node.js has a built-in module called http that you can use to create servers.
+
+```js
+const http = require("http");
+```
+
+- This http module provides a function `createServer` which is used to **listen** to incoming request.
+- This method provides the instance of a server `createServer`
+- This server can receive requests and send back responses.
---
+++ Node/5. CreatingTheServer/server.cjs
@@ -0,0 +1,18 @@
+// 1. Import the built-in 'http' module
+// Since 'http' is a native module, in newer Node versions, you can import it as 'node:http' (recommended)
+// You could also just write 'http' in older versions
+const http = require("node:http");
+
+// 2. Create an HTTP server instance
+// The 'createServer' method expects a function with (req, res) parameters
+// 'req' = incoming request object (client details)
+// 'res' = outgoing response object (what we will send back)
+const server = http.createServer(function (req, res) {
+  // 3. Send the final response to the client and end the request
+  // 'res.end()' sends the response data and closes the TCP connection
+  res.end("Hello, this is my first Node.js HTTP server!🧐");
+});
+
+// 4. Start the server and make it listen on port 777
+// Now your server will wait for requests at http://localhost:777
+server.listen(777);
