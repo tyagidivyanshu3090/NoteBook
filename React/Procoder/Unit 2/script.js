2a51e11e6e4f56ac032cfd761e06f47cdbfea96d
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-05-07 18:16:42 +0530

    Creating parent-child element

--- React/Procoder/Unit 2/script.js
+++ React/Procoder/Unit 2/script.js
@@ -1,11 +1,21 @@
-// Creating the react element
+// Creating the single react element
 
-const h2 = React.createElement(
-  "h2",
-  { className: "subHeading" },
-  "Hello React!"
-);
+// const h2 = React.createElement(
+//   "h2",
+//   { className: "subHeading" },
+//   "Hello React!"
+// );
 
+// * Creating the multiple parent-child react element -> Using the array Object
+const h2 = React.createElement("h2", { className: "subHeading" }, [
+  React.createElement(
+    "div",
+    { className: "subHeading" },
+    "This is the child Element"
+  ),
+]);
+
+console.log(h2);
 // Selecting the root element of HTML to render it -> using JavaScript
 const rootElement = document.getElementById("root");
 
