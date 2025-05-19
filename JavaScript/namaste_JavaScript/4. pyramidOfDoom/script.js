363f77b5c8a7b03fa20250ac327f1dcde9ec5889
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-05-19 18:38:36 +0530

    Creating a promise using Promise constructor

--- JavaScript/namaste_JavaScript/4. pyramidOfDoom/script.js
+++ JavaScript/namaste_JavaScript/4. pyramidOfDoom/script.js
@@ -1,7 +1,40 @@
-function onDone() {
-  console.log("All done!");
-}
+// function onDone() {
+//   console.log("All done!");
+// }
 
-// You hand over control to setTimeout:
-setTimeout(onDone, 1000);
-// You no longer decide *when* onDone() runs—setTimeout does.
+// // You hand over control to setTimeout:
+// setTimeout(onDone, 1000);
+// // You no longer decide *when* onDone() runs—setTimeout does.
+
+// * Unit 2 creating the promise
+
+const cart = ["apple", "Mango", "shampoo"];
+
+const promise = createOrder(cart);
+
+// .then method is called on the promise when it is populated with data or promise is fulfilled by createOrder function
+promise.then((orderId) => {
+  // proceedToPayment(orderId);
+  console.log(orderId);
+});
+
+/*
+  1. now createOrder function must return a promise -> to create promise we use Promise constructor
+  2. the  Promise constructor executor function which has the resolve and reject function as param
+  3. the resolve and reject are the function provided by the javaScript
+  4. if logic/functionality fails inside the promise -> we return the reject function other we return the resolve function 
+*/
+function createOrder(cart) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      if (cart.length) {
+        // if cart is valid we make a db call and return the orderID. for time being keeping ID const
+        resolve("ORDER_Id_123");
+      } else {
+        //
+        const err = new Error("Cart is not valid or empty");
+        reject(err);
+      }
+    }, 500);
+  });
+}
