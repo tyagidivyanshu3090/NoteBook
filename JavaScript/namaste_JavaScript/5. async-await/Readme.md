f9d674352edbb5182351692e1b51ecdb12a233c3
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-05-26 09:13:05 +0530

    async function always return promise

---
+++ JavaScript/namaste_JavaScript/5. async-await/Readme.md
@@ -0,0 +1,4 @@
+# Async-await
+
+- async written in front of function to make it asynchronous.
+- An async function `always returns` a Promise. If you explicitly return a non-Promise value, JavaScript will automatically wrap it in a resolved Promise.
---
+++ JavaScript/namaste_JavaScript/5. async-await/script.js
@@ -0,0 +1,8 @@
+async function getData() {
+  return "Namaste";
+}
+const dataPromise = getData();
+console.log(dataPromise); // Output: Promise {<fulfilled>: "Namaste"}
+
+// ? accessing the value of promise: Using .then method
+dataPromise.then(console.log);
