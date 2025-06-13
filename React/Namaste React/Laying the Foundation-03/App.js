81ae462ba3702f75c09c542883545e927929875c
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-06-13 09:55:07 +0530

    Injecting the react element

--- React/Namaste React/Laying the Foundation-03/App.js
+++ React/Namaste React/Laying the Foundation-03/App.js
@@ -10,12 +10,17 @@ const HeadingComponent = () => {
   );
 };
 
+// * Creating the react element using JSX and injecting into the functional component. We can also inject the react element using the react.CreateElement. React element is nothing but js object
+const reactElement = <p> This is the react element created using jsx </p>;
+console.log(reactElement); // JavaScript Object
+
 const HeadingComponent2 = () => (
   <div>
     <HeadingComponent />
     <h3> Office: National Data Center </h3>
     {/* Infusing the javaScript inside the jsx */}
     {console.log("Hello wolrd")}
+    {reactElement}
     <h4> Unit: Namaste React Functional Component </h4>
   </div>
 );
--- React/Namaste React/Laying the Foundation-03/Readme.md
+++ React/Namaste React/Laying the Foundation-03/Readme.md
@@ -7,3 +7,15 @@
 
 - In `main.js` we have used the Jsx and React core to create the react element
 - In `app.js` we are using the functional Component
+
+## what is component composition?
+
+- Component Composition is the process of combining multiple smaller components inside a larger one to build UIs in a modular and scalable way.
+
+## Injecting React element
+
+- We can inject the react element into the functional component using the brackets `{}`
+- **What is react element?** It is nothing but javaScript object.
+- **Methods to create React element?**
+  - Using `React.createElement()` : as done in lecture 1
+  - Using JSX as done in app.js file
