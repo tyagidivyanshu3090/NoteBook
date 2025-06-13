2464391a5193682d039c34e2f13232fa1f727eea
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-06-13 15:34:07 +0530

    Unable to resolve the image

--- React/Namaste React/Talk Is Cheap-04/App.js
+++ React/Namaste React/Talk Is Cheap-04/App.js
@@ -1,19 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Logo from "./assets/logo.png";
+import Logo from "./asset/Food_Logo.jpeg";
+console.log(Logo); // This should print a blob or asset path
 
 const Header = () => {
   return (
-    <div>
-      <img src={Logo} alt="" />
+    <div className="header">
+      <div className="logo-container">
+        {/* <img src={Logo} alt="Food Logo" width="200" /> */}
+        <img
+          src={require("./asset/Food_Logo.jpeg")}
+          alt="Food Logo"
+          width="200"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 };
 
 const AppLayout = () => {
+  let x = 10;
   return (
-    <div>
-      Layout
+    <div className="app">
+      {console.log("Logo:", Logo)}
       <Header />
     </div>
   );
