618216c015bfd63755991d136932fb5dda3e84fa
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-06-17 11:51:59 +0530

    Adding the css to the Restaurant card

--- React/Namaste React/Talk Is Cheap-04/App.js
+++ React/Namaste React/Talk Is Cheap-04/App.js
@@ -2,6 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Logo from "./asset/AppLogo.jpg";
 
+const RestaurantCard = () => {
+  return (
+    <div className="rest-card">
+      <img
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/jo9pdipf4elcuch8g55q"
+        alt="res-logo"
+        className="res-logo"
+      />
+      <h2> Meghna Foods </h2>
+      <h3> Briyani, North Indian, Asian</h3>
+      <h4> 4.3 ⭐️</h4>
+      <h4> 38 minutes</h4>
+    </div>
+  );
+};
+
 const Body = () => {
   return (
     <div className="body">
@@ -10,6 +26,7 @@ const Body = () => {
       </div>
       <div className="restaurant-container">
         {/*  Restaurant Card -> Reused */}
+        <RestaurantCard />
       </div>
     </div>
   );
@@ -38,6 +55,9 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <div className="">
+        <Body />
+      </div>
     </div>
   );
 };
