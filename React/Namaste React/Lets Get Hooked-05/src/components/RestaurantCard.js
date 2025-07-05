a2655407e33243028385d5778604d7476e66eeae
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-07-06 03:17:25 +0530

    Updated restaurant

--- React/Namaste React/Lets Get Hooked-05/src/components/RestaurantCard.js
+++ React/Namaste React/Lets Get Hooked-05/src/components/RestaurantCard.js
@@ -1,24 +1,26 @@
 import { CDN_URL } from "../utils/constant";
 
-const RestaurantCard = (resData) => {
-  console.log(resData.resData);
-  const { name, cuisines, avgRating, costForTwo, sla, cloudinaryImageId } =
-    resData?.resData;
+const RestaurantCard = ({ resData }) => {
+  const resList = resData;
 
-  const cuisine = cuisines.join(", ");
-  const { deliveryTime } = sla;
   return (
-    <div className="rest-card">
-      <img
-        src={`${CDN_URL}${cloudinaryImageId}`}
-        alt="res-logo"
-        className="res-logo"
-      />
-      <h2> {name} </h2>
-      <h3> {cuisine} </h3>
-      <h4> {avgRating} ⭐️</h4>
-      <h4>{costForTwo}</h4>
-      <h4> {deliveryTime} minutes</h4>
+    <div className="restaurant-container">
+      {resData.map((item) => {
+        return (
+          <div className="rest-card" key={item?.info?.id}>
+            <img
+              src={`${CDN_URL}${item?.info?.cloudinaryImageId}`}
+              alt="res-logo"
+              className="res-logo"
+            />
+            <h2>{item?.info?.name}</h2>
+            <h3>{item?.info?.cuisines?.join(", ")}</h3>
+            <h4>{item?.info?.avgRating} ⭐️</h4>
+            <h4>{item?.info?.costForTwo}</h4>
+            <h4>{item?.info?.sla?.deliveryTime} minutes</h4>
+          </div>
+        );
+      })}
     </div>
   );
 };
