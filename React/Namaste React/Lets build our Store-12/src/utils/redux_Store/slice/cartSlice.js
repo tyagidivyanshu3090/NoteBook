f42f9b692a2cdef5488de01a559c17268887553f
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-08-01 09:21:23 +0530

    redux

--- React/Namaste React/Lets build our Store-12/src/utils/redux_Store/slice/cartSlice.js
+++ React/Namaste React/Lets build our Store-12/src/utils/redux_Store/slice/cartSlice.js
@@ -1,5 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  items: [],
+};
+
 const cartSlice = createSlice({
-    
-})
+  name: "cart",
+  initialState,
+    reducers: {
+        addItem: (state, action) => {
+        state.items.push(action.payload);
+        },
+        removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.id !== action.payload.id);
+        },
+        clearCart: (state) => {
+        state.items = [];
+        },
+    },
+});
+
