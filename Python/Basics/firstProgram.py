9740fa2c50b31e3db2ecf62afac7531734d2b617
DivyanshuTyagi <tyagidivyanshu3090@gmail.com>
2025-09-08 11:29:41 +0530

    Python string indexing and length method

---
+++ Python/Basics/firstProgram.py
@@ -0,0 +1,20 @@
+# String methods:
+
+# length of string -> len() method
+sentence = "Hello World!"
+print(len(sentence)) # Output: 12 (includes the space and exclamation mark)
+
+# ------------------------------------------
+
+# indexing:
+s = "Python"
+
+# Positive indexing
+print(s[0])   # Output: P
+print(s[2])   # Output: t
+
+# Negative indexing
+print(s[-1])  # Output: n
+print(s[-3])  # Output: h
+
+# ------------------------------------------
