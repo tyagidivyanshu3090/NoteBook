362f988338eb4cb5681bce066335c1b541286ba1
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-10-02 16:00:31 +0530

    Python indexing

--- Python/Basics/firstProgram.py
+++ Python/Basics/firstProgram.py
@@ -1,20 +1,27 @@
 # String methods:
+# String Multi line using triple brackets:
 
-# length of string -> len() method
-sentence = "Hello World!"
-print(len(sentence)) # Output: 12 (includes the space and exclamation mark)
+# str = """ Hello Divyanshu
+# Welcome to python world"""
+# print(str)
+
 
 # ------------------------------------------
+# length of string -> len() method
 
+# sentence = "Hello World!"
+# print(len(sentence)) # Output: 12 (includes the space and exclamation mark)
+
+# ------------------------------------------
 # indexing:
-s = "Python"
 
-# Positive indexing
-print(s[0])   # Output: P
-print(s[2])   # Output: t
+# s = "Python"
+# # Positive indexing
+# print(s[0])   # Output: P
+# print(s[2])   # Output: t
 
-# Negative indexing
-print(s[-1])  # Output: n
-print(s[-3])  # Output: h
+# # Negative indexing
+# print(s[-1])  # Output: n
+# print(s[-3])  # Output: h
 
 # ------------------------------------------
