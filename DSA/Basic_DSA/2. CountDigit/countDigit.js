edcfa5639f5d69e56e5a8032f881cee23ccc7d5d
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-11-02 16:13:00 +0530

    CornerCase Handling

--- DSA/Basic_DSA/2. CountDigit/countDigit.js
+++ DSA/Basic_DSA/2. CountDigit/countDigit.js
@@ -1,16 +1,27 @@
 // Counting the number of digit in the number
 // To solve the question while loop is used
 
-let input = 1974;
-let digitCount = 0;
-while (input > 0) {
-  // input = input / 10; // When you divide an integer in JavaScript, you can get a decimal (a floating-point number).
-  //  * JavaScript division (/) keeps the decimal.
-  input = Math.floor(input / 10);
-  digitCount++;
+function countDigit(number) {
+  // handling corner case:
+  if (number === 0) return 1;
+
+  // 2. Handle the "Negative" corner case
+  // Convert the number to positive using Math.abs()
+  let num = Math.abs(n);
+
+  let count = 0;
+  while (number > 0) {
+    // input = input / 10; // When you divide an integer in JavaScript, you can get a decimal (a floating-point number).
+    //  * JavaScript division (/) keeps the decimal.
+    number = Math.floor(number / 10);
+    count++;
+  }
+  return count;
 }
 
-console.log("Number of digit is -> ", digitCount);
+let input = 1974;
+
+console.log("Number of digit is -> ", countDigit(input));
 
 // Extra: Short way
 // let input = 1974;
