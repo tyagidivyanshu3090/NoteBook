b431aac174da1de8c86aeba30abfcfb9a11f3e92
DivyanshuTyagi <tyagidivyanshu3090@gmail.com>
2025-12-10 17:28:33 +0530

    feat: Add script to take and print user name and age.

---
+++ Python/Basics/main4.py
@@ -0,0 +1,8 @@
+# Taking the input from the user 
+    # Input is always string  
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Printing the output
+print("Hello", name)
+print("You are", age, "years old")
