d5edc1be059c2e2759bd9416a851165f2f0ec662
DivyanshuTyagi <tyagidivyanshu3090@gmail.com>
2025-12-10 12:33:03 +0530

    feat: include math and random operations examples

--- Python/Basics/main2.py
+++ Python/Basics/main2.py
@@ -28,3 +28,24 @@ print(type(list)) # <class 'list'>
 print(tuple)    # (1, 2, 3, 4, 5)
 
 # -------------------------------------------------------------------
+
+# Math and random:
+
+import math   # <--- unlock the toolbox
+import random # <--- unlock the random toolbox
+
+number = 5.7
+
+# --- Built-in Tools (No import needed) ---
+print("Round:", round(number))  # Output: 6
+print("Max:", max(5, 10, 2))    # Output: 10
+print("Abs:", abs(-50))         # Output: 50
+
+# --- Tools from 'math' Module ---
+print("Floor:", math.floor(number)) # Output: 5
+print("Ceil:", math.ceil(number))   # Output: 6
+print("Sqrt:", math.sqrt(25))       # Output: 5.0
+print("Pi:", math.pi)               # Output: 3.14159...
+
+# --- Tools from 'random' Module ---
+print("Random:", random.random())   # Output: 0.123... (random number 0-1)
