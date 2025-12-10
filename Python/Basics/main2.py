ba0c73b9db38431aa42643673d45fde24010ef80
DivyanshuTyagi <tyagidivyanshu3090@gmail.com>
2025-12-10 11:33:09 +0530

    feat: add Python example demonstrating basic data types, lists, and tuples.

---
+++ Python/Basics/main2.py
@@ -0,0 +1,30 @@
+# Q: 1 Data type and there type function value:
+
+a = 10
+print(type(a)) # <class 'int'>  
+
+b = 10.5
+print(type(b)) # <class 'float'>
+
+c = "Hello"
+print(type(c)) # <class 'str'>
+
+d = True
+print(type(d)) # <class 'bool'>
+
+e = None
+print(type(e)) # <class 'NoneType'>
+
+# -------------------------------------------------------------------
+
+# List and tuple:
+
+list = [1, 2, 3, 4, 5]
+tuple = (1, 2, 3, 4, 5)
+
+print(type(list)) # <class 'list'>
+
+# tuple[0] = 10 # TypeError: 'tuple' object does not support item assignment
+print(tuple)
+
+# -------------------------------------------------------------------
