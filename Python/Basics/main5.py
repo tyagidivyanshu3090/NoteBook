9cdd044ddcdd800650a4f727423d717e9b655106
DivyanshuTyagi <tyagidivyanshu3090@gmail.com>
2025-12-11 17:42:51 +0530

    feat: add string slicing examples and remove minor whitespace from string looping example.

--- Python/Basics/main5.py
+++ Python/Basics/main5.py
@@ -8,4 +8,4 @@ print('The value at index 1 is:', name[1])
 # Looping the string
 print("Looping the string:")
 for char in name:
-    print(char)
+    print(char) 
---
+++ Python/Basics/main6.py
@@ -0,0 +1,23 @@
+# Slicing the string
+
+
+# Length - Negative Number = Positive Index.
+fruit = "Pomogranate"
+print(fruit[-5:-2]) # Output: "ana"    
+# length - negative number = positive index 
+    # 11 - (5) = 6 // I.e. start from a 
+    # 11 - (2) = 9 // I.e. end at t not included
+
+fruit = "Mango"
+print(fruit[0:3]) # Output: "Man"
+print(fruit[:4]) # Output: "Mang"
+print(fruit[2:]) # Output: "ngo"
+print(fruit[-2:]) # Output: "go"    // 5-2 = 3
+
+
+
+
+
+
+
+
