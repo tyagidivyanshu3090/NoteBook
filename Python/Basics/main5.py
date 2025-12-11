9647400424ce2655b520bc8e2a0243fb0c26270b
DivyanshuTyagi <tyagidivyanshu3090@gmail.com>
2025-12-11 12:17:00 +0530

    example: Add basic Python string manipulation demonstration.

---
+++ Python/Basics/main5.py
@@ -0,0 +1,11 @@
+# String Unit:
+
+name = "Divyanshu"
+
+# Accessing the value at index
+print('The value at index 1 is:', name[1])
+
+# Looping the string
+print("Looping the string:")
+for char in name:
+    print(char)
