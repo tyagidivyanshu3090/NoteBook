cc78b507254c36527f25619896871b362d7c3cb4
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-12-18 23:07:22 +0530

    docs: Add JavaScript notes and examples for the array `slice()` method.

---
+++ JavaScript/JS_Notes/Slice_Method/slice.js
@@ -0,0 +1,23 @@
+// Slice method
+
+// The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included). The original array will not be modified.
+let fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+
+let newFruits = fruits.slice(1, 4);
+
+console.log(newFruits); // Output: ["banana", "cherry", "date"]
+
+console.log(fruits); // Output: ["apple", "banana", "cherry", "date", "elderberry"]
+
+// ------------------------------------------------------------------
+
+// 1.  Creating the copy using slice method
+
+let sliceCopy = fruits.slice();
+
+console.log(sliceCopy); // Output: ["apple", "banana", "cherry", "date", "elderberry"]
+
+// ------------------------------------------------------------------
+
+
+
